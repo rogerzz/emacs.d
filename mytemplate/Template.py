c6a321f26361b02cbdbf6fe82a1f71af1cf6a4c4
rogerzz <rogerzhongzhang@gmail.com>
2014-07-22 13:16:16 +0800

    current

---
+++ mytemplate/Template.py
@@ -0,0 +1,14 @@
+################################################################################                                                                                                                              
+#                                                                                                                                                                                                             
+# Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved                                                                                                                                                      
+#                                                                                                                                                                                                             
+################################################################################  
+"""                                                                                                                                                                                                           
+This module 
+                                                                                                                                                                                                              
+Authors: zhangzhong01(zhangzhong01@baidu.com)                                                                                                                                                                 
+Date:    2014/04/05 17:23:06                                                                                                                                                                                  
+"""
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
